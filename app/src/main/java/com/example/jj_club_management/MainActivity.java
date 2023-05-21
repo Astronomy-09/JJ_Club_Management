954d2cd375703022d22eae4f7b2ed521ce0fb20d
minji9988 <rnwjf111@daum.net>
2023-05-21 19:51:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jj_club_management/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.jj_club_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public
+class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected
+    void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 로그인 페이지(activity_main)에 있는 '회원가입하러가기' 텍스트 클릭 시
+        // RegisterActivity(회원가입 페이지)로 이동
+        TextView txtRegister = (TextView) findViewById(R.id.registerTextButton);
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick( View view) {
+                Intent intent = new Intent(MainActivity.this , RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+}
