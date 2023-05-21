b431fecce17b9a4f862f439ea04ca4b8d39eb032
minji9988 <rnwjf111@daum.net>
2023-05-21 20:03:13 +0900

    테스트중

--- app/src/main/java/com/example/jj_club_management/RegisterActivity.java
+++ app/src/main/java/com/example/jj_club_management/RegisterActivity.java
@@ -12,5 +12,6 @@ class RegisterActivity extends AppCompatActivity {
     void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
